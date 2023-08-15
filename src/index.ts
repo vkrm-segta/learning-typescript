7657535a1b34298c64a59b4c0e19f8a76ad2660c
Vikram Segta <vikramsegta43@gmail.com>
2023-08-15 23:57:35 +0530

    instence type

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,21 @@
-// Primitive types
+// ----------------------------------------------------------------
+//  Primitive types
+// ----------------------------------------------------------------
 
-  const weight: number = 52.4;
-  const intelligent: boolean = true;
-  const Name: string = 'Vikram segta';
-  
-  const undefinedValue: undefined = undefined;
-  const nullValue: null = null;
-  
-  const star: symbol = Symbol('star');
-  const bigg: bigint = 24n;
+//   const weight: number = 52.4;
+//   const intelligent: boolean = true;
+//   const Name: string = 'Vikram segta';
+
+//   const undefinedValue: undefined = undefined;
+//   const nullValue: null = null;
+
+//   const star: symbol = Symbol('star');
+//   const bigg: bigint = 24n;
+
+// ----------------------------------------------------------------
+// Instance types
+// ----------------------------------------------------------------
+
+let regExp: RegExp = new RegExp('abcdefghijklmnopqrstuvwxyzABCDEF');
+let array: Array<number> = [1, 2, 3];
+let set: Set<number> = new Set([1, 2, 1, 3]);
