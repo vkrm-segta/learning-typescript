fc60d19aab2c9ee9d824aab57ab984ddcf1759f3
Vikram Segta <vikramsegta43@gmail.com>
2023-08-15 23:45:31 +0530

    primitives values

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,11 @@
-const message = 'Hello world';
-console.log(message);
+// Primitive types
+
+  const weight: number = 52.4;
+  const intelligent: boolean = true;
+  const Name: string = 'Vikram segta';
+  
+  const undefinedValue: undefined = undefined;
+  const nullValue: null = null;
+  
+  const star: symbol = Symbol('star');
+  const bigg: bigint = 24n;
